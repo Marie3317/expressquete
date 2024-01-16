a6e1dcfc2475ba9d962c655320081c9da9c5c705
Marie <131977082+Marie3317@users.noreply.github.com>
2024-01-16 09:34:14 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+
+// Ajout des routes pour les movies
+const movieControllers = require("./controllers/movieControllers");
+
+app.get("/api/movies", movieControllers.getMovies);
+app.get("/api/movies/:id", movieControllers.getMovieById);
+app.post("/api/movies", movieControllers.postMovie);
+app.put("/api/movies/:id", movieControllers.updateMovie);
+
+// Ajout des routes pour les utilisateurs
+const userControllers = require('./controllers/userControllers');
+
+app.get('/api/users', userControllers.getUsers);
+app.get('/api/users/:id', userControllers.getUserById);
+app.post("/api/users", userControllers.postUsers);
+app.put("/api/movies/:id", userControllers.updateUsers);
+
+module.exports = app;
